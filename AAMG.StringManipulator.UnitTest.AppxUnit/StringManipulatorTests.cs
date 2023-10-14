d7676cf5e08671ed105c8217e2e003b410506acd
adonismolinao3 <adonismolinao3@outlook.com>
2023-10-14 15:54:45 -0600

    Agregar archivos de proyecto.

---
+++ AAMG.StringManipulator.UnitTest.AppxUnit/StringManipulatorTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AAMG.StringManipulator.UnitTest.AppxUnit
+{
+    public class StringManipulatorTests
+    {
+        [Fact]
+        public void ReverseString_ShouldReverseString()
+        {
+            // Arrange
+            string input = "Hello, World!";
+            string expected = "!dlroW ,olleH";
+            var manipulator = new StringManipulator();
+
+            // Act
+            string result = manipulator.ReverseString(input);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void RemoveSpaces_ShouldRemoveSpaces()
+        {
+            // Arrange
+            string input = "Hello, World!";
+            string expected = "Hello,World!";
+            var manipulator = new StringManipulator();
+
+            // Act
+            string result = manipulator.RemoveSpaces(input);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ AAMG.StringManipulator/StringManipulator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AAMG.StringManipulator
+{
+    public class StringManipulator
+    {
+        public string ReverseString(string input)
+        {
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        public string RemoveSpaces(string input)
+        {
+            return input.Replace(" ", string.Empty);
+        }
+    }
+}
